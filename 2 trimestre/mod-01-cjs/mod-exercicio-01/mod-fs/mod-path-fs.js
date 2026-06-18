0c53ec6dd833b5ae3388036a15923c825c15fced
Emillyelo2008 <emillyjanuario2409@gmail.com>
2026-06-18 11:33:43 +0000

    modulos

--- 2 trimestre/mod-01-cjs/mod-exercicio-01/mod-fs/mod-path-fs.js
+++ 2 trimestre/mod-01-cjs/mod-exercicio-01/mod-fs/mod-path-fs.js
@@ -2,8 +2,15 @@
 const fs = require('fs')
 const path = require('node:path')
 
-// resultado ler arquivo caminho pasta arquivo codificador 
-// const arquivo = fs.readFileSync(path.join(__dirname, "/README.MD"), "utf-8")
-const arquivo=fs.readFileSync(path.resolve(__dirname, "/README.MD") , "utf-8")
-console.log(arquivo)
+
+try {
+    // resultado ler arquivo caminho pasta arquivo codificador 
+    // const arquivo = fs.readFileSync(path.join(__dirname, "/README.MD"), "utf-8")
+    const arquivo = fs.readFileSync(path.resolve(__dirname, "/README.MD"), "utf-8")
+    console.log(arquivo)
+} catch (error) {
+    console.error('Deu ruim...')
+    console.log(error.message)
+
+}
 
