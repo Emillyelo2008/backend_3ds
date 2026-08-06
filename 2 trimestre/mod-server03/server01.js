dce68c182b998cba4ca98adf2badafefaed6b809
Emillyelo2008 <emillyjanuario2409@gmail.com>
2026-08-06 13:29:01 +0000

    Adicionar servidor HTTP básico e configuração de rotas

---
+++ 2 trimestre/mod-server03/server01.js
@@ -0,0 +1,13 @@
+//  Importar módulos
+const http = require ('node:http')
+
+
+// criar servidor e rotas
+const server = http.createServer( (req, res)=> {
+// rotas e conteúdo
+})
+
+// liberar porta no meu PC
+server.listen(PerformanceTiming, () => {
+    console.log(`Servidor rodando em http://localhost:${porta}`)
+})
